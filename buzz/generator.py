cf030e56c19a2147bc8869b899c5fff18f161a4d
Linh Nguyen Vu <nguyenvulinh99@gmail.com>
2021-06-09 17:49:39 +0700

    Update test 09/06

--- buzz/generator.py
+++ buzz/generator.py
@@ -6,7 +6,7 @@ buzz = ('continuous testing', 'continuous integration',
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
     'seriously')
-nouns = ('KHTN', '20HCB1', 'MMTNC')
+nouns = ('HCMUS', '20HCB1', 'MMTNC')
 
 def sample(l, n = 1):
     result = random.sample(l, n)
